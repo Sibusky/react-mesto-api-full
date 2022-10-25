6707c1b6787df819d4e55259b80aab5c76467ee4
Alexey Smirnov <Sibusky@yandex.ru>
2022-10-25 15:06:54 +0400

    refactor: remove res.data from handleCheckToken

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -215,7 +215,7 @@ function App() {
       let jwt = localStorage.getItem("jwt");
       Auth.getContent(jwt)
         .then((res) => {
-          const { _id, email } = res.data;
+          const { _id, email } = res;
           setLoggedIn(true);
           setUserData({ _id, email });
         })
